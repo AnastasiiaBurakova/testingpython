ad1f02e2c0ea0674190fed0d2dbecfb831f532ef
AnastasiiaBurakova <99281254+AnastasiiaBurakova@users.noreply.github.com>
2022-04-08 10:34:19 +0300

    Add files via upload

--- test9.py
+++ test9.py
@@ -1,6 +1,7 @@
+from math import sqrt 
 def is_prime(n):
     if n > 1:    
-        for i in range (2, int(n/2)+1):
+        for i in range (2, int(sqrt(n))+1):
             if (n % i) == 0:
                 return False
         else:
@@ -9,6 +10,18 @@ def is_prime(n):
         return False   
 
 def primes_below(n):
-    for i in range (2, n + 1):
+    l = []
+    for i in range (2, n):
         if is_prime(i):
-            print (i, end = ' ')       
+            l.append(i)
+    return l           
+
+def palindrome_primes():
+    l = []
+    for i in range (10000, 100000):
+        if str(i) == str(i)[::-1]:
+            if is_prime(i):
+                l.append(i)
+    return l
+
+
